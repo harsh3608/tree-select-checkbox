26f9ae04587b5809ac24122fd975003d453eae71
Harsh Patel <harsh3608@gmail.com>
2023-12-15 14:55:52 +0530

    initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        loadChildren: () => import('./task/task.module').then(m=>m.TaskModule)
+    }
+];
